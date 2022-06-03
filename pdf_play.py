2e21294821151deb99a565c6acd57f5e9a672902
Alexander Shain <alexandershain.5@gmail.com>
2022-06-03 23:15:48 +0300

    run_pdf_parsing

--- pdf_play.py
+++ pdf_play.py
@@ -1,39 +1,115 @@
+import os
 import PyPDF2
-import sys
-
 from PyPDF2 import PdfFileReader
-from pdfminer.layout import LTTextContainer
+from pdfminer.layout import LTTextBoxHorizontal, LTTextBoxVertical
 from pdfminer.high_level import extract_pages
+from shapely.geometry import Polygon
+from tqdm import tqdm
+import json
+
+
+IOU_THRESHOLD = 0.5
+
+
+def get_annotations(page):
+    anno_bboxes = []
+    if '/Annots' in page:
+        for annot in page["/Annots"]:
+            obj = annot.getObject()
+            if '/C' in obj:
+                assert '/Rect' in obj
+                anno_rect = obj['/Rect']
+                if isinstance(anno_rect, list):
+                    if not isinstance(anno_rect[0], PyPDF2.generic.FloatObject) and not isinstance(anno_rect[0], PyPDF2.generic.NumberObject):
+                        print('HAS EXCEPTION')
+                        print(type(anno_rect[0]))
+                        continue
+                anno_bboxes.append(anno_rect)
+    return anno_bboxes
+
+
+def convert_bbox_to_poly(bbox):
+    x0, y0, x1, y1 = bbox
+    poly = [[x0, y0], [x1, y0], [x1, y1], [x0, y1]]
+    poly = Polygon(poly)
+    return poly
+
+
+def calculate_iou(poly_1, poly_2):
+    iou = poly_1.intersection(poly_2).area / poly_1.union(poly_2).area
+    return iou
+
+
+def get_texts(page_layout, anno_bboxes):
+    anno_polys = [convert_bbox_to_poly(bbox) for bbox in anno_bboxes]
+    is_matched = [False] * len(anno_polys)
+    texts = []
+    bboxes = []
+    bboxes_inds = []
+    for element in page_layout:
+        if isinstance(element, LTTextBoxHorizontal) or isinstance(element, LTTextBoxVertical):
+            bbox_poly = convert_bbox_to_poly(element.bbox)
+
+            for a_ind, a_poly in enumerate(anno_polys):
+                if not is_matched[a_ind]:
+                    iou = calculate_iou(bbox_poly, a_poly)
+                    if iou > IOU_THRESHOLD:
+                        is_matched[a_ind] = True
+                        texts.append(element.get_text())
+                        bboxes.append(element.bbox)
+                        bboxes_inds.append(a_ind)
+                else:
+                    continue
+
+    return texts, bboxes, bboxes_inds
 
 
 def play(path):
     reader = PdfFileReader(path)
 
-    for j, (page_layout, page) in enumerate(zip(extract_pages(path), reader)):
-        print('*****', j)
-        for element in page_layout:
-            print(element)
+    doc_id = os.path.basename(path)
+    doc_id = os.path.splitext(doc_id)[0]
 
-    #pdfminer
-    #print(text)
-    #exit(0)
-        # pdfminer.high_level.extract_text_to_fp(file, sys.stdout)
+    document_annotations = {
+        'texts': [],
+        'bboxes': [],
+        'bboxes_inds': [],
+        'anno_bboxes': [],
+        'doc_id': doc_id,
+    }
+
+    for j, (page_layout, page) in enumerate(zip(extract_pages(path), reader.pages)):
+        # GET ALL ANNOTATIONS FROM PAGE:
+        anno_bboxes = get_annotations(page)
+
+        all_texts, all_bboxes, bboxes_inds = [], [], []
+
+        if len(anno_bboxes) > 0:
+            all_texts, all_bboxes, bboxes_inds = get_texts(page_layout, anno_bboxes)
+
+        document_annotations['texts'].append(all_texts)
+        document_annotations['bboxes'].append(all_bboxes)
+        document_annotations['bboxes_inds'].append(bboxes_inds)
+        document_annotations['anno_bboxes'].append(anno_bboxes)
+
+    return document_annotations
 
 
+def prepare_data(data_folder):
+    all_files = os.listdir(data_folder)
+    all_files = sorted([f for f in all_files if f.endswith('.pdf')])
 
-    for i, page in enumerate(reader.pages):
-        print('PAGE', i)
-        # text = page.extractText()
-        # print(text)
-        try:
-            for annot in page["/Annots"]:
-                print(annot.getObject())  # (1)
-                print("")
-        except:
-            # there are no annotations on this page
-            pass
+    result = []
+
+    for f in tqdm(all_files, total=len(all_files)):
+        anno = play(os.path.join(data_folder, f))
+        result.append(anno)
+
+    with open('result.json', 'w') as f:
+        json.dump(result, f)
 
 
 if __name__ == '__main__':
-    path = 'D:\Downloads\si_izm\si\data_1\pdfs\\2005-30815-05.pdf'
-    play(path)
+    path = 'D:\Downloads\si_izm\si\data_1\pdfs'
+    prepare_data(path)
+    #play(path)
